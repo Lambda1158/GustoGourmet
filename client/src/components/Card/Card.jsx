99ec449865662c8cc4367198d9aac46ca3bde888
bruno <brunoherrera1158@gmail.com>
2022-09-08 17:59:58 -0300

    lala

--- client/src/components/Card/Card.jsx
+++ client/src/components/Card/Card.jsx
@@ -19,12 +19,14 @@ export default function Card({ title, image, diets, puntuacion, id, createdInBd 
                 <div className="t2"><img className="img" src={image} alt="img not found" /></div>
                 <div className="t1"><h3 className="card_title" >{title}</h3></div>
                 <span >Puntuacion: {puntuacion} </span>
-                {diets ? diets.map((e) => {
-                    return <p>{e}</p>
-                }) : <p key={id}>No Diets :P</p>}
+                <div className="dietas">
+                    {diets ? diets.map((e) => {
+                        return <p>{e}</p>
+                    }) : <p key={id}>No Diets :P</p>}
+                </div>
 
                 <Link className="link" to={`/detail/${id},${createdInBd ? 1 : 0}`}><button className="btn">More info</button></Link>
-                <button value={id} className="btn btn-outline" onClick={e => handleDelete(e)}>X</button>
+                <button value={id} className="btn b1" onClick={e => handleDelete(e)}>X</button>
 
             </div>
         </div>
