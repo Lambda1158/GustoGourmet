6f132e530ef48bda038a7cbaf38ec964526fa624
bruno <brunoherrera1158@gmail.com>
2023-06-05 17:59:51 -0300

    cambios en las card

--- client/src/components/Card/Card.jsx
+++ client/src/components/Card/Card.jsx
@@ -14,20 +14,19 @@ export default function Card({ title, image, diets, puntuacion, id, createdInBd 
     }
     return (
         <div className="card">
-            <div className="body">
-                <div className="t2"><img className="img" src={image} alt="img not found" /></div>
-                <div className="t1"><h3 className="card_title" >{title}</h3></div>
-                <div className="dietas">
+                <img className="img" src={image} alt="img not found" />
+                <div className="card-content">
+                <h3>{title}</h3>
                     <p className="relleno-card" >Puntuacion: {puntuacion} </p>
                     {diets ? diets.map((e) => {
                         return <p className="relleno-card">{e}</p>
                     }) : <p className="relleno-card" key={id}>No Diets :P</p>}
+               
+                <div className="btn-container">
+                <Link className="link" to={`/detail/${id},${createdInBd ? 1 : 0}`}><button className="btn btn-primary">More info</button></Link>
+                <button value={id} className="btn btn-secondary" onClick={e => handleDelete(e)}>X</button>
                 </div>
-
-                <Link className="link" to={`/detail/${id},${createdInBd ? 1 : 0}`}><button className="btn">More info</button></Link>
-                <button value={id} className="btn b1" onClick={e => handleDelete(e)}>X</button>
-
-            </div>
+                </div>
         </div>
 
 
