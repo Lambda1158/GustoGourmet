efd8de066606d76037b8fded1049c2c2aef6de21
bruno <brunoherrera1158@gmail.com>
2023-07-26 15:28:00 -0300

    packagejson

---
+++ client/babel.config.js
@@ -0,0 +1,12 @@
+// babel.config.js
+module.exports = {
+    presets: [
+      "@babel/preset-env",
+      "@babel/preset-react"
+      // Otros presets que puedas necesitar...
+    ],
+    plugins: [
+      // Aquí puedes agregar plugins de Babel si los necesitas...
+    ]
+  };
+  
