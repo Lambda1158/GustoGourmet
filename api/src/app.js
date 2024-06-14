2d25937ff3fa98d6db9d81e8d7f78fbc20707fa2
bruno <brunoherrera1158@gmail.com>
2024-06-14 14:17:55 -0300

    fix en el cors

--- api/src/app.js
+++ api/src/app.js
@@ -10,25 +10,31 @@ require("./db.js");
 const server = express();
 
 server.name = "API";
-server.use(cors());
+const corsOptions = {
+  origin: "https://recipes-app-486l.onrender.com",
+  credentials: true,
+};
+
+server.use(cors(corsOptions));
 server.use(express.static(join(__dirname, "./uploads")));
 server.use(express.urlencoded({ extended: true, limit: "50mb" }));
 server.use(express.json({ limit: "50mb" }));
 server.use(cookieParser());
 server.use(morgan("dev"));
-server.use((req, res, next) => {
-  res.header(
-    "Access-Control-Allow-Origin",
-    "https://recipes-app-486l.onrender.com"
-  ); // update to match the domain you will make the request from
-  res.header("Access-Control-Allow-Credentials", "true");
-  res.header(
-    "Access-Control-Allow-Headers",
-    "Origin, X-Requested-With, Content-Type, Accept"
-  );
-  res.header("Access-Control-Allow-Methods", "GET, POST, OPTIONS, PUT, DELETE");
-  next();
-});
+
+// server.use((req, res, next) => {
+//   res.header(
+//     "Access-Control-Allow-Origin",
+//     "https://recipes-app-486l.onrender.com"
+//   ); // update to match the domain you will make the request from
+//   res.header("Access-Control-Allow-Credentials", "true");
+//   res.header(
+//     "Access-Control-Allow-Headers",
+//     "Origin, X-Requested-With, Content-Type, Accept"
+//   );
+//   res.header("Access-Control-Allow-Methods", "GET, POST, OPTIONS, PUT, DELETE");
+//   next();
+// });
 
 server.use(routes);
 // Use this after the variable declaration
